441f7a79763c180e15c42994de402d01855eb1be
esirpraceeissahpray-6 <esirpraceeissahpray@gmail.com>
2026-04-30 10:57:01 +0300

    Update reportRoutes.js

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,36 +1,20 @@
 // routes/reportRoutes.js
 
 const express = require("express");
-
 const router = express.Router();
 
 /*
 Import Report Controller
-
-This connects routes to logic.
 */
-
-const {
-  submitReport
-} = require("../controllers/reportController");
+const { submitReport } = require("../controllers/reportController");
 
 /*
 Report Submission Route
 
-Endpoint:
+Final Endpoint:
 POST /api/reports
-
-Purpose:
-Allows users to submit scam reports
-from the frontend to the backend.
 */
 
-router.post("/reports", submitReport);
-
-/*
-Export Router
-
-Allows server.js to use this route.
-*/
+router.post("/", submitReport);
 
 module.exports = router;
